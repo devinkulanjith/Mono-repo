cabb027ddb2863a0dcda21974339eb9b561b7f2c
devin <devin.gallage@clouda.io>
2022-10-24 15:50:02 +0530

    test bash

--- release.sh
+++ release.sh
@@ -4,8 +4,11 @@ dir=$PWD
 
 apps=$(<read_file.txt)
 
-for name in $apps;
-do
- cd $dir/$name
- gnome-terminal --wait -- vtex release pre stable
-done
+echo node --v
+echo yarn --v
+
+#for name in $apps;
+#do
+# cd $dir/$name
+# gnome-terminal  -- vtex release pre stable
+#done
