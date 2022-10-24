cf879c9dfa3c0ccfae14c2017d0fe27b81a4bd57
devin <devin.gallage@clouda.io>
2022-10-24 16:03:30 +0530

    solve bug

--- release.sh
+++ release.sh
@@ -4,11 +4,9 @@ dir=$PWD
 
 apps=$(<read_file.txt)
 
-echo node --v
-echo yarn --v
 
-#for name in $apps;
-#do
-# cd $dir/$name
-# gnome-terminal  -- vtex release pre stable
-#done
+for name in $apps;
+do
+ cd $dir/$name
+ echo "yes" | vtex release patch stable
+done
