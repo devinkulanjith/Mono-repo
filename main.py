07a810ec9a946b9f548d075c127ce5e34bbb2d1c
devin <devin.gallage@clouda.io>
2022-10-27 15:54:28 +0530

    Add some test comments

--- main.py
+++ main.py
@@ -6,33 +6,34 @@ from signal import SIGKILL
 from os import kill
 
 subprocess.Popen("vtex use testautomation", shell=True)
-sleep(5)
+sleep(10)
 
 currentDirectory = os.getcwd()
-subDirectoryList = ["b2b-accelerator-dealer-map", "b2b-accelerator-consumer-store-theme"]
 
 apps = open('read_file.txt','r')
 appList = apps.readlines()
 
 def appLink():
+    subprocess.Popen("echo enter 4", shell=True)
     cmd = "vtex link > output.txt&"
     subprocess.Popen(cmd, stdout= True, shell=True)
 
 
 for app in appList:
     os.chdir(currentDirectory + '/' + app.replace("\n",""))
+    subprocess.Popen("echo enter 1", shell=True)
     subprocess.Popen("touch output.txt", shell=True)
     sleep(3)
     process = Process(target= appLink)
     process.start()
     var = True
-
+    subprocess.Popen("echo enter 2", shell=True)
     while var:
         with open('output.txt', 'r', encoding='utf-8') as file:
             contents = file.read()
 
             sentence = 'App linked successfully'
-
+            subprocess.Popen("echo enter 3", shell=True)
             result = contents.find(sentence)
             if result != -1:
                 var = False
