4bfabcea4852ec0e7fe2b36b83a229bd156ec922
devin <devin.gallage@clouda.io>
2022-11-03 16:49:30 +0530

    Add workflow item

---
+++ deployment.py
@@ -0,0 +1,27 @@
+# import os
+# import subprocess
+
+
+# currentDirectory = os.getcwd()
+
+# apps = open('president_order.txt','r')
+# appListOrder = apps.readlines()
+
+# vtexAppLinkOrder = []
+
+# subprocess.Popen("vtex use testautomation", shell=True)
+
+
+# appListString = os.environ.get("CHANGED_DIRECTORIES")
+
+# appList = appListString.split('[')[1].split(']')[0].split(',')
+
+# if len(appList) != 0:
+#     for app in appListOrder:
+#        appName = app.replace('\n','')
+#        if appName in appList:
+#            vtexAppLinkOrder.append(appName)
+
+#     for app in vtexAppLinkOrder:
+#         os.chdir(currentDirectory + '/' + app)
+        
