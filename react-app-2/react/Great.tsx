387bd3288e57b225881a4cc910641a58f7da8e19
devin <devin.gallage@clouda.io>
2022-11-03 17:13:14 +0530

    Add content to interfaces file

---
+++ react-app-2/react/Great.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+type Props = {
+  name: string
+}
+
+function Greet({ name }: Props) {
+  return (
+    <div>
+       <div>Hey, {name}</div>
+       <div>Hey, {name}</div>
+    </div>
+  )
+}
+
+export default Greet
